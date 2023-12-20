be598720515a7a04ee64310ac2c41b8831d4df3a
Ashton Torgerud <ashtorged@outlook.com>
2023-12-20 01:34:00 -0600

    Added Video Functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Museum from './Museum';
+import Navbar from "./Navbar";
 import MuseumVideos from './MuseumVideos';
 
 function App() {
   return (
+    
     <Router>
+      { <Navbar /> }
       <Routes>
-      <Route path='/' element={<Museum/>} />
+      <Route path='/'  />
       <Route path="/MuseumVideos" element={<MuseumVideos/>} />
         {/* Add more routes for other subpages */}
       </Routes>
--- src/MuseumVideos.js
+++ src/MuseumVideos.js
@@ -2,55 +2,174 @@ import Navbar from "./Navbar";
 import "./MuseumVideos.css";
 import TestVideo from './TestVideo.mp4';
 import VideoItem from './VideoItem';
+import { useState, useRef, useEffect  } from "react";
 
 
 function MuseumVideos() {
 
+  /// Use States & Variable definitions
+
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [buttonText, setButtonText] = useState('Play');
+  const [muteButtonText, setMuteButtonText] = useState('Mute');
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(100);
+  const [muted, setMuted] = useState(false);
 
 
+  /// Volume Changing Code
 
-  var myVideo = document.getElementById("video1"); 
-  var playButton = document.getElementById("playButton");
+  const handleVolumeChange = (event) => {
+    const newVolume = parseInt(event.target.value);
+    const video = videoRef.current;
+    video.volume = newVolume / 100;
+    setVolume(newVolume);
+    setDuration(parseInt(videoRef.current.duration));
+  };
 
-  function playPause() {
-    if (typeof myVideo !== "undefined" && myVideo !== null) {
-      if (myVideo.paused) {
-        myVideo.play();
-      } else {
-        myVideo.pause();
-      }
+  const handleVolumeMute = () => {
+    const video = videoRef.current;
+    if (video.muted) {
+      video.muted = false;
+      setMuteButtonText('Mute');
     } else {
-      console.log("myVideo is undefined or null");
+      video.muted = true;
+      setMuteButtonText('Muted');
     }
+
   }
 
 
-  /*function playPause() { 
-    if (myVideo.paused) 
-      myVideo.play(); 
-    else 
-      myVideo.pause(); 
-  } */
 
+  /// Change the video seaking
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(videoRef.current.currentTime);
+  };
+
+  const handleLoadedMetadata = () => {
+    console.log("Absolute Gigabanger");
+    setDuration(parseInt(videoRef.current.duration));
+  };
+
+  const handleSeekChange = (e) => {
+    const seekTime = e.target.value;
+    videoRef.current.currentTime = seekTime;
+    setCurrentTime(seekTime);
+  };
+
+
+
+
+  /// Pause Video
+
+  const handlePlayPause = () => {
+    const video = videoRef.current;
+    if (isPlaying) {
+      video.pause();
+    } else {
+      video.play();
+    }
+    setIsPlaying(!isPlaying);
+
+    if (buttonText === 'Play')
+    setButtonText('Paused');
+  else
+    setButtonText('Play');
+  };
+
+
+/// Handle Video Fullscreening 
+
+const handleFullscreen = () => {
+  const video = videoRef.current;
+  video.requestFullscreen();
+}
+
+
+  /// Use Effect to update the volume slider and seeking slider
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const handleSeekUpdate = () => {
+      setCurrentTime(video.currentTime);
+    };
+
+
+
+    const handleVolumeUpdate = () => {
+      setVolume(Math.round(video.volume * 100));
+    };
+
+    video.addEventListener("timeupdate", handleSeekUpdate);
+    video.addEventListener("volumechange", handleVolumeUpdate);
+
+    return () => {
+      video.removeEventListener("timeupdate", handleSeekUpdate);
+      video.removeEventListener("volumechange", handleVolumeUpdate);
+    };
+  }, []);
+
+
+
+
+  /// Speed Control Dropdown Menu
+
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const SetPlayBackSpeed = (e) => {
+    const video = videoRef.current;
+    console.log(e);
+    video.playbackRate = e;
+  }
+
+
+  
+  // Options for the dropdown
+  const options = ['0.25', '0.5', '0.75', '1', '1.25', '1.5', '1.75', '2'];
 
 
   return (
     <div className="root">
-      <Navbar />
 
       <div className="flex container">
 
         <div className="video-panel">
-          <video id="video1" controls>
-            <source src={TestVideo}/>
+          <video id="video1" ref={videoRef} controls>
+            <source src={TestVideo} volume={volume / 100} muted={false} onTimeUpdate={ handleTimeUpdate } onLoadedMetadata={handleLoadedMetadata} />
             Your browser does not support the video tag.
             <div className="video-elements">
               <h2>Your browser does not support videos. Get rekt, L, bozo, touch grass you absolute dingis</h2>
             </div>
 
           </video>
+
+          <button onClick={handlePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
+          <button onClick={handleVolumeMute}>{muteButtonText}</button>
+          <button onClick={handleFullscreen}>{'Fullscreen'}</button>
+          <input type="range" id="VolumeSlider" min="0" max="100" value={ volume } onChange={handleVolumeChange}/>
+          <input type="range" id="SeekerSlider" min="0" max={ duration+1 } value={ currentTime } onChange={handleSeekChange}/>
           <h1>[TITLE PLACEHOLDER]</h1>
-          <button onClick={playPause} id="playButton">Play</button> 
+
+
+          <select
+            value={selectedOption}
+            onChange={(e) => {
+              setSelectedOption(e.target.value);
+              SetPlayBackSpeed(e.target.value);
+            }}
+          >
+            <option value="">1</option>
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
         </div>
 
         <div className="up-next">
@@ -61,33 +180,13 @@ function MuseumVideos() {
                 <li>
                   <VideoItem />
                 </li>
-                <li>
-                  <VideoItem />
-                </li>
-                <li>
-                  <VideoItem />
-                </li>
-                <li>
-                  <VideoItem />
-                </li>
-                <li>
-                  <VideoItem />
-                </li>
-                <li>
-                  <VideoItem />
-                </li>
               </ul>
             </div>
           </div>
         </div>
-
       </div>
-
-
-
     </div>
   );
-
 }
 
 export default MuseumVideos;
