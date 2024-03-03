165ecf3c7911a59854ea44c4ca65697a3942638d
Ashton Torgerud <ashtorged@outlook.com>
2024-03-03 14:39:27 -0600

    Added Hash Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as HashRouter, Routes, Route,  } from 'react-router-dom';
+import { HashRouter as HashRouter, Routes, Route,  } from 'react-router-dom';
 import Museum from './Museum';
 import Schedule from './Schedule';
 import Navbar from "./Navbar";
