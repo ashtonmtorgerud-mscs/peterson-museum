d1deb1ef96ae07abc92a96bfddb16f5540ba964c
Ashton Torgerud <ashtorged@outlook.com>
2024-03-10 14:13:38 -0500

    Fixed mistakes

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,11 @@ function Footer() {
 
     return (
       <div className="root-footer" >
-        <h3>Developed by <a href='https://www.linkedin.com/in/ashton-torgerud-a3b54025b/'>Ashton Torgerud</a> colaboration with Gail Boyum and John Dar.</h3>
+        <h3>Special thanks to Gail Boyum and John Erickson.</h3>
         <p>For information and questions about the museum please contact johndar@acegroup.cc</p>
-        <p>For information and questions about the website please contact ashtorged@outlook.com </p>
+        <br></br>
+        <p>For information and questions about the website please contact 507-875-22415 or email petersoncity@gmail.com</p>
+        <p>Developed by <a href='https://www.linkedin.com/in/ashton-torgerud-a3b54025b/'>Ashton Torgerud</a></p>
       </div>
     );
   }
